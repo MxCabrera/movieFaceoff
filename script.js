4256d090b8a1f51887d69831dcc10d7b1700aa91
Aleks <nikolic.aleks90@gmail.com>
2020-04-15 17:55:52 -0400

    API call is successful, wired submit button to store user input, setup an init function and streamlined our document ready

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let movieApp = {}
 
+movieApp.userResponse;
+
 movieApp.key = '3628ebe0c40080ef9275f966a8eaaa92'
 
-// Document Ready
-$(document).ready(function () {
+movieApp.url = 'https://api.themoviedb.org/3/movie'
 
+// Declaring globally the poster value of our object
+movieApp.poster;
+
+movieApp.init = function () {
     // listen for click on the how to play button to display alert 
     $('button.howTo').on('click', function (e) {
         e.preventDefault();
@@ -15,40 +20,49 @@ $(document).ready(function () {
     });
 
     // listen for click on Let's Play button to scroll down the game area
-    $(function () {
-        $("button.letsPlay").on('click', function () {
-            $('html').animate({
-                scrollTop: $('main.gameArea').offset().top
-            }, 'slow');
-            return false;
-        });
+    $("button.letsPlay").on('click', function () {
+        $('html').animate({
+            scrollTop: $('main.gameArea').offset().top
+        }, 'slow');
+        return false;
     });
 
+    // Create this in the global scope let user response =
     $('#submitAnswer').on('submit', function (event) {
         event.preventDefault()
-        let userResponse = $('#userAnswer').val()
-        console.log(userResponse);
+        movieApp.userResponse = $('#userAnswer').val()
+        movieApp.callVariable(movieApp.userResponse)
     })
 
+    movieApp.getMovieById('238')
+}
 
-    movieApp.getData = () => {
-        $.ajax({
-            url: "https://api.themoviedb.org/3/search/movie",
-            // https://api.themoviedb.org/3/movie/ - secondary URL that allows us to search for a movies 'id'. The Godfathers is 238.
-            method: 'GET',
-            dataType: 'json',
-            data: {
-                api_key: movieApp.key,
-                query: "The Godfather"
-                // Trying to pass in userResponse here but scope issue
-            }
-        }).then((result) => {
-            const movieYear = result
-            // Current search returns a bunch of results with multiple movies and multiple properties for each movie like release_year - unsuccessfully been trying to access it by result.release_year and release[0] to target first item with index 0
-            console.log(movieYear);
-        })
-    }
+movieApp.callVariable = function (variable) {
+    console.log(variable);
+    // User response YYYY ==== movieYear.release_date splice off month/day and just get the YYYY
+}
 
-    movieApp.getData()
+movieApp.getMovieById = (movieId) => {
+    $.ajax({
+        url: `${movieApp.url}/${movieId}`,
 
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            api_key: movieApp.key,
+
+        }
+    }).then((result) => {
+        const movieYear = result.release_date
+        console.log(result);
+        console.log(movieYear);
+        let moviePoster = result.poster_path
+        // Logging our results.poster_path to see we are getting the end of the poster url
+        console.log(moviePoster);
+    })
+}
+
+// Document Ready
+$(document).ready(function () {
+    movieApp.init()
 });

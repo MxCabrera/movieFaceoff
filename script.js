b2f4f4a0aab364e86a997398b29e144a8a342250
Aleks <nikolic.aleks90@gmail.com>
2020-04-15 08:53:12 -0400

    Added API calls and stored user input form in a scope limited variable

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+let movieApp = {}
+
+movieApp.key = '3628ebe0c40080ef9275f966a8eaaa92'
+
+// Document Ready
 $(document).ready(function () {
 
-// listen for click on the how to play button to display alert 
+    // listen for click on the how to play button to display alert 
     $('button.howTo').on('click', function (e) {
         e.preventDefault();
         alert(`
@@ -9,12 +14,41 @@ $(document).ready(function () {
         If you guess wrong, you can always skip or guess again!`)
     });
 
-// listen for click on Let's Play button to scroll down the game area
+    // listen for click on Let's Play button to scroll down the game area
     $(function () {
         $("button.letsPlay").on('click', function () {
-            $('html').animate({ scrollTop: $('main.gameArea').offset().top }, 'slow');
+            $('html').animate({
+                scrollTop: $('main.gameArea').offset().top
+            }, 'slow');
             return false;
         });
     });
+
+    $('#submitAnswer').on('submit', function (event) {
+        event.preventDefault()
+        let userResponse = $('#userAnswer').val()
+        console.log(userResponse);
+    })
+
+
+    movieApp.getData = () => {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/movie",
+            // https://api.themoviedb.org/3/movie/ - secondary URL that allows us to search for a movies 'id'. The Godfathers is 238.
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                api_key: movieApp.key,
+                query: "The Godfather"
+                // Trying to pass in userResponse here but scope issue
+            }
+        }).then((result) => {
+            const movieYear = result
+            // Current search returns a bunch of results with multiple movies and multiple properties for each movie like release_year - unsuccessfully been trying to access it by result.release_year and release[0] to target first item with index 0
+            console.log(movieYear);
+        })
+    }
+
+    movieApp.getData()
 
 });
